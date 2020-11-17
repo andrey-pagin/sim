2082a0431046e4038465b5b2b24c65a99890d381
swg <pagin.andrey.98@gmail.com>
2020-11-17 10:49:30 +0300

    Added execution from the script and manual speed changing.

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ except:
 
 class App(Frame):
     def __init__(self, root):
+
         super(App, self).__init__(root)
         self.root = root
         self.pack()
@@ -26,26 +27,45 @@ class App(Frame):
         self.root.background_label = Label(root, image=self.image)
         self.root.background_label.place(x=0, y=0, relwidth=1, relheight=1)
         self.root.iconbitmap('images/coppeliaSim.ico')
+        self.root.resizable(False, False)
+
+        # Simulation parameters
         self.clientID = -1
+        self.rightMotor = -1
+        self.leftMotor = -1
+        self.noseSensor = -1
+        self.min_max_speed = [50 * 3.141592/180, 300 * 3.141592/180]
+        self.speed = (self.min_max_speed[0] + self.min_max_speed[1]) * 0.5
+        self.back_until_time = 0
+        self.normal = False
+
+        # Scale settings
+        self.left = Scale(self.root, from_=10, to=-10, orient=VERTICAL, command=self.set_left_motor_speed)
+        self.right = Scale(self.root, from_=10, to=-10, orient=VERTICAL, command=self.set_right_motor_speed)
 
         # Redefine X button
         self.root.protocol('WM_DELETE_WINDOW', self.quit)
 
         # Text settings.
-        self.text = Label(root, text='', font='arial 10')
+        self.text = Label(self.root, text='', font='arial 10')
 
         # Adding buttons.
-        self.button_connect = Button(root, text='connect', width=10, height=2, bg='green', font='arial 18',
+        self.button_connect = Button(self.root, text='connect', width=10, height=2, bg='green', font='arial 18',
                                      command=self.connect)
-        self.button_start = Button(root, text='start', width=10, height=2, bg='white', font='arial 18',
+        self.button_start = Button(self.root, text='start', width=10, height=2, bg='white', font='arial 18',
                                    command=self.start_simulation)
-        self.button_stop = Button(root, text='stop', width=10, height=2, bg='white', font='arial 18',
+        self.button_stop = Button(self.root, text='stop', width=10, height=2, bg='white', font='arial 18',
                                   command=self.stop_simulation)
-        self.button_getvalue = Button(root, text='get velocity', width=10, height=2, bg='white', font='arial 18',
+        self.button_getvalue = Button(self.root, text='get velocity', width=10, height=2, bg='white', font='arial 18',
                                       command=self.get_value)
+        self.button_normal_execution = Button(self.root, text='normal execution', width=12, height=2, bg='blue',
+                                              font='arial 18', command=self.normal_execution)
         # Create a grid and put buttons there.
         self.button_connect.pack(side='top', expand=1)
+        self.button_normal_execution.pack(side='top', expand=1)
         self.text.pack(side='bottom', expand=1)
+        self.right.pack(side='right', expand=1)
+        self.left.pack(side='right', expand=1)
         self.button_start.pack(side='left', expand=1)
         self.button_stop.pack(side='left', expand=1)
         self.button_getvalue.pack(side='left', expand=1)
@@ -61,6 +81,15 @@ class App(Frame):
             res, objs = sim.simxGetObjects(self.clientID, sim.sim_handle_all, sim.simx_opmode_blocking)
             if res == sim.simx_return_ok:
                 print('Number of objects in the scene: ', len(objs))
+                self.text.config(text='Connected to remote API.', bg='green')
+
+                code, self.leftMotor = sim.simxGetObjectHandle(self.clientID, "bubbleRob_leftMotor",
+                                                         sim.simx_opmode_oneshot_wait)
+                code, self.rightMotor = sim.simxGetObjectHandle(self.clientID, "bubbleRob_rightMotor",
+                                                          sim.simx_opmode_oneshot_wait)
+                code, self.noseSensor = sim.simxGetObjectHandle(self.clientID, "bubbleRob_sensingNose",
+                                                          sim.simx_opmode_oneshot_wait)
+
             else:
                 self.text.config(text='Remote API function call returned with error code: ' + str(res), bg='red')
 
@@ -69,7 +98,10 @@ class App(Frame):
     def start_simulation(self):
         if self.clientID != -1:
             if 0 <= sim.simxStartSimulation(self.clientID, sim.simx_opmode_oneshot) <= 1:
-                self.text.config(text='Simulation started.', bg='white')
+                self.text.config(text='Simulation started', bg='white')
+                sim.simxReadProximitySensor(self.clientID, self.noseSensor, sim.simx_opmode_streaming)
+                sim.simxSetJointTargetVelocity(self.clientID, self.leftMotor, 0, sim.simx_opmode_streaming)
+                sim.simxSetJointTargetVelocity(self.clientID, self.rightMotor, 0, sim.simx_opmode_streaming)
             else:
                 self.text.config(text='Problem with starting simulation', bg='red')
 
@@ -77,23 +109,54 @@ class App(Frame):
         if self.clientID != -1:
             if 0 <= sim.simxStopSimulation(self.clientID, sim.simx_opmode_oneshot) <= 1:
                 self.text.config(text='Simulation stopped', bg='white')
+                self.normal = False
             else:
                 self.text.config(bg='black')
 
     def get_value(self):
-        res, v0 = sim.simxGetObjectHandle(self.clientID, 'bubbleRob', sim.simx_opmode_oneshot_wait)
+        res, v0 = sim.simxGetObjectHandle(self.clientID, 'leftMotor', sim.simx_opmode_oneshot_wait)
         res, linear_velocity, angular_velocity = sim.simxGetObjectVelocity(self.clientID, v0,
                                                                            sim.simx_opmode_oneshot_wait)
         self.text.config(text=str(linear_velocity))
-        sim.simxSetObjectFloatParameter(self.clientID, v0, sim.sim_shapefloatparam_init_velocity_x, 10000,
-                                        sim.simx_opmode_oneshot_wait)
+
+    def set_left_motor_speed(self, var):
+        if not self.normal:
+            speed = self.left.get()
+            sim.simxSetJointTargetVelocity(self.clientID, self.leftMotor, speed, sim.simx_opmode_streaming)
+
+    def set_right_motor_speed(self, var):
+        if not self.normal:
+            speed = self.right.get()
+            sim.simxSetJointTargetVelocity(self.clientID, self.rightMotor, speed, sim.simx_opmode_streaming)
 
     def quit(self):
         # Before closing the connection to CoppeliaSim, make sure that the last command sent out had time to arrive.
         # You can guarantee this with (for example):
+        self.normal = False
         sim.simxGetPingTime(self.clientID)
 
         # Now close the connection to CoppeliaSim:
         sim.simxFinish(self.clientID)
         sys.exit(0)
 
+    def normal_execution(self):
+        if self.normal:
+            self.normal = False
+            sim.simxSetJointTargetVelocity(self.clientID, self.leftMotor, 0, sim.simx_opmode_streaming)
+            sim.simxSetJointTargetVelocity(self.clientID, self.rightMotor, 0, sim.simx_opmode_streaming)
+        else:
+            self.normal = True
+        while self.normal:
+            result = sim.simxReadProximitySensor(self.clientID, self.noseSensor, sim.simx_opmode_buffer)
+            if result[1] > 0:
+                self.back_until_time = time.time() + 4
+            if self.back_until_time < time.time():
+                sim.simxSetJointTargetVelocity(self.clientID, self.leftMotor, self.speed, sim.simx_opmode_streaming)
+                sim.simxSetJointTargetVelocity(self.clientID, self.rightMotor, self.speed, sim.simx_opmode_streaming)
+            else:
+                sim.simxSetJointTargetVelocity(self.clientID, self.leftMotor, -self.speed / 2,
+                                               sim.simx_opmode_streaming)
+                sim.simxSetJointTargetVelocity(self.clientID, self.rightMotor, -self.speed / 8,
+                                               sim.simx_opmode_streaming)
+            #self.root.update_idle_tasks()
+            self.root.update()
